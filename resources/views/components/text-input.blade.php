4f1af99f3709668fa8275d6532ccbb66e81f86c1
AU <>
2024-08-31 08:52:03 +0800

    commit on laptop before merging agency_user_integration

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,7 +1,7 @@
 @props(['id' => null, 'name' => '', 'value' => '', 'disabled' => false])
 
 @php
-    $attributes = $attributes->class('h-12 border w-96 border-custom-fields focus:border-custom-dark-blue rounded-md shadow-sm')
+    $attributes = $attributes->class('h-12 border w-96 border-custom-fields focus:border-custom-dark-blue rounded-md shadow-sm p-4')
                             ->merge(['id' => $id, 'name' => $name, 'value' => $value]);
 @endphp
 
